f2074232cc7ca2d2f7f34ec5d92605c916b997ca
OJHA Ayush <ayush.ojha@soprasteria.com>
2019-02-18 06:49:08 +0530

    build folder added to git

---
+++ client/build/precache-manifest.e45d6b93d08ecd9503bdb8f7207ef8da.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "23e90d1f5d20a7998e5aeeb9a0ca24e5",
+    "url": "/static/media/img5.23e90d1f.jpg"
+  },
+  {
+    "revision": "83288211bc8b06f82cac",
+    "url": "/static/css/main.d0fa35ae.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7b418b95265465c6304ed0404386e011",
+    "url": "/static/media/instagram.7b418b95.png"
+  },
+  {
+    "revision": "1e5eb3c63b4b98653725",
+    "url": "/static/js/2.1e5eb3c6.chunk.js"
+  },
+  {
+    "revision": "9320392cbdcc56c4c7c9fa5c314df6e2",
+    "url": "/static/media/profile1.9320392c.jpg"
+  },
+  {
+    "revision": "4a7428b9cd950d888a8e67e92acccdc3",
+    "url": "/static/media/profile2.4a7428b9.jpg"
+  },
+  {
+    "revision": "4891608383c2bbf9a9dbe62b937ddb92",
+    "url": "/static/media/img1.48916083.jpg"
+  },
+  {
+    "revision": "783c335d55e519edc8fca04a12c2f1dd",
+    "url": "/static/media/img2.783c335d.jpg"
+  },
+  {
+    "revision": "5121e1df9745cdf193ff66e32c431846",
+    "url": "/static/media/img3.5121e1df.png"
+  },
+  {
+    "revision": "1771f52bf6eb45ff175a8d9f6ac03acd",
+    "url": "/static/media/img4.1771f52b.png"
+  },
+  {
+    "revision": "83288211bc8b06f82cac",
+    "url": "/static/js/main.83288211.chunk.js"
+  },
+  {
+    "revision": "441936b4e4cc8ad11c268d5f89df06bc",
+    "url": "/static/media/img6.441936b4.jpg"
+  },
+  {
+    "revision": "0576a290be2f6287676f0e4b842b8be3",
+    "url": "/static/media/docker.0576a290.png"
+  },
+  {
+    "revision": "bcb36bfbd2209958514bdcaa9ef24097",
+    "url": "/static/media/javascript.bcb36bfb.png"
+  },
+  {
+    "revision": "ecc6f9e5d804ae0db7490c683af2f406",
+    "url": "/static/media/mongo.ecc6f9e5.png"
+  },
+  {
+    "revision": "fb715d72022219140fce8e3931088bbf",
+    "url": "/static/media/nodejs.fb715d72.png"
+  },
+  {
+    "revision": "37fe8322b169ddbdeabf75930e886ac6",
+    "url": "/static/media/react.37fe8322.png"
+  },
+  {
+    "revision": "9cf4a14cb5024b5d6e988c1519570ce3",
+    "url": "/static/media/html.9cf4a14c.png"
+  },
+  {
+    "revision": "04cf122be0afe7542083d1a3967390ae",
+    "url": "/static/media/css.04cf122b.png"
+  },
+  {
+    "revision": "05da3431471058c803afacb2f5483f7b",
+    "url": "/static/media/linkedin.05da3431.png"
+  },
+  {
+    "revision": "1e5eb3c63b4b98653725",
+    "url": "/static/css/2.d94d4306.chunk.css"
+  },
+  {
+    "revision": "e96276e7f8379f157358a7207dad55c3",
+    "url": "/index.html"
+  }
+];
